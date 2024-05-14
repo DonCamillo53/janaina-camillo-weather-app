eb21a188bd976b0e97f0b9d62621a40d41aecbbd
Camillo Hochberg <camillo.h@hotmail.de>
2024-05-14 16:36:55 +0200

    add a Form-Component

---
+++ janaina-camillo-weather-app/src/components/Form.jsx
@@ -0,0 +1,26 @@
+export function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onAddActivity(data);
+
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add a new Activity</h2>
+      <div>
+        <label htmlFor="activity_input">Name</label>
+        <input type="text" id="activity_input" name="name" />
+      </div>
+      <div>
+        <label htmlFor="activity_checkbox">Good-Weather Activity</label>
+        <input type="checkbox" id="activity_checkbox" name="isForGoodWeather" />
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
+}
