77703d782afa90d77850dfaa3b77a7b220f6ae64
Janaina Voigt <jv.de@icloud.com>
2024-05-15 22:13:20 +0200

    Added the category list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,24 @@
 export default function List({ activities, weatherStatus, onDeleteActivity }) {
   console.log("activities,", activities);
 
+  const icons = {
+    outdoor_activities: " 🌳",
+    sports: " ⚽️",
+    arts_and_crafts: " 🎨",
+    cooking_and_baking: " 👨🏽‍🍳",
+    fitness_and_wellness: " 🏋🏻‍♂️",
+    entertainment: " 🎭",
+    educational: " 📚",
+  };
+
   return (
     <ul>
       {activities.map((activity) => {
         if (activity.isForGoodWeather === weatherStatus) {
           return (
             <li key={activity.id} id={activity.id}>
-              {activity.name}{" "}
+              {activity.name}
+              <span>{icons[activity.category]}</span>
               <button onClick={() => onDeleteActivity(activity.id)}>x</button>
             </li>
           );
