f264a79f1889260bdd90fd06758778795563e129
Janaina Voigt <jv.de@icloud.com>
2024-05-15 10:01:47 +0200

    Added the data variable

--- janaina-camillo-weather-app/src/App.jsx
+++ janaina-camillo-weather-app/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import { Form } from "./components/Form";
+import Form from "./components/Form";
 
 function App() {
+  const [activities, setactivities] = useState();
+
   return <Form />;
 }
 export default App;
--- janaina-camillo-weather-app/src/components/Form.jsx
+++ janaina-camillo-weather-app/src/components/Form.jsx
@@ -1,10 +1,14 @@
-export function Form({ onAddActivity }) {
+export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
 
-    onAddActivity(data);
+    let data = {
+      name: event.target.elements.name.value,
+      isForGoodWeather: event.target.elements.isForGoodWeather.checked,
+    };
+
+    //onAddActivity(data);
+    console.log(data);
 
     event.target.reset();
     event.target.elements.name.focus();
