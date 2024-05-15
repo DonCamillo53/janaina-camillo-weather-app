9f205e8457034346734919c2c3b43b49dc400d4d
Janaina Voigt <jv.de@icloud.com>
2024-05-15 13:14:02 +0200

    Added the useEffect

--- janaina-camillo-weather-app/src/App.jsx
+++ janaina-camillo-weather-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import { uid } from "uid";
@@ -9,17 +9,35 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    async function startFetching() {
+      try {
+        const response = await fetch(
+          `https://example-apis.vercel.app/api/weather`
+        );
+        const weather = await response.json();
+        setWeather(weather);
+      } catch (e) {
+        console.error(e.message);
+      }
+    }
+    startFetching();
+  }, []);
+  console.log(weather);
+  let isGoodWeather = weather.isGoodWeather;
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }]);
   }
-  const isGoodWeather = false;
 
   // const filteredActivites = activities.filter(
   //   (activity) => activity.isForGoodWeather === isGoodWeather
   // );
 
   console.log(activities);
+
   return (
     <>
       <List activities={activities} prop={isGoodWeather} />
